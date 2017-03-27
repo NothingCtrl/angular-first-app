073793f9d28ce645c954f727b271969ff1e047ec
NothingCtrl <nothingctrl@gmail.com>
2017-03-27 18:55:13 +0700

    Session 2 Lecture 37

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -14,6 +14,9 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName: string = '';
   serverCreated: boolean = false;
+  servers = ['TestServer', 'TestServer2'];
+  toggle = false;
+  logs = [];
 
   constructor() {
     setTimeout(() => {
@@ -26,11 +29,31 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onClickButton() {
+    this.toggle = !this.toggle;
+    this.logs.push(new Date());
+  }
+
+  logDecor(value, isStyle = false) {
+    let count = 0;
+    for (let log in this.logs) {
+      count += 1;
+      if (this.logs[log] == value) {
+        if (count > 4) {
+          return isStyle ? 'green' : true;
+        } else {
+          return isStyle ? 'none' : false;
+        }
+      }
+    }
   }
 
 
